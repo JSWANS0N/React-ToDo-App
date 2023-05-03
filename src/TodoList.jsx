73741f5524b94efb042741aaab4d90c430dc273d
Jacob Swanson <swanson.jacob08@gmail.com>
2023-05-02 22:51:38 -0500

    Add feature to store todo list in localstorage

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,19 +2,20 @@ import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
     return (
-        <ul className="list">
-        {todos.length === 0 && "No Todos"}
-        {
-            todos.map(todo => {
-                return (
-                    <TodoItem 
-                        {...todo} 
-                        key={todo.id} 
-                        toggleTodo={toggleTodo} 
-                        deleteTodo={deleteTodo} />
-                );
-            })
-        }
-        </ul>
+        <>
+            <h1 className="header">Todo List</h1>
+            <ul className="list">
+                {todos.length === 0 && "No Todos"}
+                {todos.map(todo => {
+                    return (
+                        <TodoItem 
+                            {...todo} 
+                            key={todo.id} 
+                            toggleTodo={toggleTodo} 
+                            deleteTodo={deleteTodo} />
+                    );
+                })}
+            </ul>
+        </>
     );
 }
