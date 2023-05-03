5586d63713d8b16fb0dd6309fcd2c827ea3c40c5
Jacob Swanson <swanson.jacob08@gmail.com>
2023-05-02 22:23:15 -0500

    Break Todo List Item into component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,4 @@
+import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
     return (
@@ -6,20 +7,11 @@ export function TodoList({ todos, toggleTodo, deleteTodo }) {
         {
             todos.map(todo => {
                 return (
-                    <li key={todo.id}>
-                        <label>
-                            <input type="checkbox" 
-                            checked={todo.completed}
-                            onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-                            {todo.title}
-                        </label>
-                        <button
-                            className="btn btn-danger"
-                            onClick={() => deleteTodo(todo.id)}
-                        >
-                            Delete
-                        </button>
-                    </li>
+                    <TodoItem 
+                        {...todo} 
+                        key={todo.id} 
+                        toggleTodo={toggleTodo} 
+                        deleteTodo={deleteTodo} />
                 );
             })
         }
